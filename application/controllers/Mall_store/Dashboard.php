ab12d564ce97b7f34bddb8394f6ca4843f3b2b2e
hdanarola <hda@narola.email>
2018-05-23 17:38:03 +0530

    Worked on Lofin for Store / Mall and Country Admin Login

---
+++ application/controllers/Mall_store/Dashboard.php
@@ -0,0 +1,24 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard
+        extends MY_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->data['title'] = $this->data['page_header'] = 'Dashboard';
+    }
+
+    /*
+     * Dashboard
+     */
+
+    public function index() {
+        $this->data['page'] = 'dashboard_page';
+        $this->data['page_header'] = 'Dashboard';
+
+        $this->template->load('user', 'Superadmin/Dashboard/index', $this->data);
+    }
+
+}
