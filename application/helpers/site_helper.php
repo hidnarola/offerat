db4b9c80e3710f20815f85d18d0c0321449125c7
hdanarola <hda@narola.email>
2018-05-24 14:42:57 +0530

    Manage Role for Suepr Admin, Country Admin and Store / Mall User

--- application/helpers/site_helper.php
+++ application/helpers/site_helper.php
@@ -30,19 +30,30 @@ function is_logged_in() {
     $loggedin_user_type = $CI->session->userdata('loggedin_user_type');
     $segment1 = $CI->uri->segment(1);
     if ($loggedin_user_type == SUPER_ADMIN_USER_TYPE && in_array($segment1, array('super-admin', 'superadmin'))) {
-        echo 1111;
-//        return TRUE;
+        return TRUE;
     } elseif ($loggedin_user_type == COUNTRY_ADMIN_USER_TYPE && in_array($segment1, array('country-admin', 'countryadmin'))) {
-        echo 22222;
-//        return TRUE;
+        return TRUE;
     } elseif ($loggedin_user_type == STORE_OR_MALL_ADMIN_USER_TYPE && in_array($segment1, array('mall-store-user', 'mall_store_user'))) {
-        echo 3333;
-//        return TRUE;
-    } 
-//    elseif (in_array($loggedin_user_type, array(SUPER_ADMIN_USER_TYPE, COUNTRY_ADMIN_USER_TYPE, STORE_OR_MALL_ADMIN_USER_TYPE)) && in_array($segment1, array('login', 'forgot-password', 'reset-password'))) {
-//        echo 4444;
-////        redirect('super-admin/dashboard');
-//    } else {
-////        redirect('')
-//    }
+        return TRUE;
+    } elseif (in_array($loggedin_user_type, array(SUPER_ADMIN_USER_TYPE, COUNTRY_ADMIN_USER_TYPE, STORE_OR_MALL_ADMIN_USER_TYPE)) && in_array($segment1, array('login', 'forgot-password', 'reset-password'))) {
+        dashboard_redirect($loggedin_user_type);
+    } elseif ((!in_array($segment1, array('super-admin', 'superadmin')) && $loggedin_user_type == SUPER_ADMIN_USER_TYPE) || (!in_array($segment1, array('country-admin', 'countryadmin')) && $loggedin_user_type == COUNTRY_ADMIN_USER_TYPE) || (!in_array($segment1, array('mall-store-user', 'mall_store_user')) && $loggedin_user_type == STORE_OR_MALL_ADMIN_USER_TYPE)) {
+        dashboard_redirect($loggedin_user_type);
+    } elseif (!in_array($loggedin_user_type, array(SUPER_ADMIN_USER_TYPE, COUNTRY_ADMIN_USER_TYPE, STORE_OR_MALL_ADMIN_USER_TYPE)) && in_array($segment1, array('super-admin', 'superadmin', 'country-admin', 'countryadmin', 'mall-store-user', 'mall_store_user'))) {
+        dashboard_redirect($loggedin_user_type);
+    }
+}
+
+function dashboard_redirect($loggedin_user_type = NULL) {
+    $redirect_path = '';
+    if ($loggedin_user_type == SUPER_ADMIN_USER_TYPE)
+        $redirect_path = 'super-admin/dashboard';
+    elseif ($loggedin_user_type == COUNTRY_ADMIN_USER_TYPE)
+        $redirect_path = 'country-admin/dashboard';
+    elseif ($loggedin_user_type == STORE_OR_MALL_ADMIN_USER_TYPE)
+        $redirect_path = 'mall-store-user/dashboard';
+    else
+        $redirect_path = '/';
+
+    redirect($redirect_path);
 }
